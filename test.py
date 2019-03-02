5894e2ee949636c347708e70884951fa3bd72ce7
Anton Antonchik <antonchikas@gmail.com>
2019-03-03 01:05:38 +0300

    test

---
+++ test.py
@@ -0,0 +1,21 @@
+from loader import DataLoader
+from data_utils import show_image, open_image
+
+
+train_images_dir = \
+    "X:\\DataScience\\human-protein-atlas-image-classification\\train"
+train_labels_file = \
+    "X:\\DataScience\\human-protein-atlas-image-classification\\train.csv"
+
+
+if __name__ == "__main__":
+    print('start')
+    loader = DataLoader(train_images_dir, train_labels_file)
+    files = loader.labels.keys()
+    for file in files:
+        colors = ['red', 'green', 'blue', 'yellow']
+        print(loader.labels[file])
+        for color in colors:
+            _image = loader.images[file][color]
+            show_image(open_image(_image))
+    print('finish')
