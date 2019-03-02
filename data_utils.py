a42ad33716e3f82dad9446a4aeb45c15de05adb4
Anton Antonchik <antonchikas@gmail.com>
2019-03-02 23:33:04 +0300

    Upload

---
+++ data_utils.py
@@ -0,0 +1,60 @@
+import os
+from PIL import Image
+import cv2 as cv
+import csv
+
+file_path = r"X:\DataScience\human-protein-atlas-image-classification\train.csv"
+dir_path = r"X:\DataScience\human-protein-atlas-image-classification\train"
+
+
+def open_image(image_file: str, opencv=False):
+    img = Image.open(image_file) if not opencv else _open_image_cv(image_file)
+    return img
+
+
+def show_image(image: Image, opencv=False):
+    image.show() if not opencv else _show_image_cv('image', image)
+
+
+def _open_image_cv(image_file: str):
+    img = cv.imread(image_file)
+    return img
+
+
+def _show_image_cv(window_name: str, image):
+    cv.imshow(window_name, image)
+
+
+def read_labels(file=file_path):
+    labels_dict = {}
+    with open(file, "r") as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            if row[1].isalpha():                    # skip title row
+                continue
+            labels = row[1].split(' ')
+            one_hot_labels = [0 for _ in range(28)]
+            for label in labels:
+                one_hot_labels[int(label)] = 1
+            labels_dict[row[0]] = one_hot_labels
+    return labels_dict
+
+
+def read_images(directory=dir_path):
+    files = sorted(os.listdir(directory))
+    images = {}
+    for file in files:
+        splited = file.split('_')
+        image, color = splited[0], splited[1][:-4]
+        # pic = open_image('\\'.join([directory, file]))
+        pic = '\\'.join([directory, file])
+        if image not in images:
+            images.update({image: {color: pic}})
+        else:
+            images[image].update({color: pic})
+    return images
+
+
+# read_images()
+
+# print(read_labels())
---
+++ loader.py
@@ -0,0 +1,28 @@
+
+import data_utils as du
+
+
+class DataLoader:
+    def __init__(self, images_dir: str, labels_file: str):
+        self.images_dir = images_dir
+        self.labels_file = labels_file
+        self.images = {}
+        self.labels = {}
+        self.read_data()
+        print(self.images, self.labels)
+
+    def read_data(self):
+        self.images = du.read_images(self.images_dir)
+        self.labels = du.read_labels(self.labels_file)
+
+    def get_data(self):
+        return self.images, self.labels
+
+
+# a = DataLoader(
+# #     r"X:\DataScience\human-protein-atlas-image-classification\train",
+# #     r"X:\DataScience\human-protein-atlas-image-classification\train.csv"
+# # )
+
+
+
